4f0ca06b7b7640bdf9a336b2d2c8d5b0ad1b4737
Nirantarjot1 <95531047+Nirantarjot1@users.noreply.github.com>
2021-12-15 18:36:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+   
+    var updated_Image = image[i];
+    
+    var updated_Name = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updated_Image;
+    document.getElementById("family_member_name").innerHTML = updated_Name;
+}
